8e9f99e9c7e5bf022e07f43d75569f388c34e05a
Evan Mao <evanzmao@gmail.com>
2024-07-04 16:05:52 -0700

    Added node and express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname+"/web"));
+
+app.get("/", function (req, res) {
+    res.sendFile("index.html");
+});
+
+app.listen(3000, function () {
+    console.log("Server is running on localhost:3000");
+});
